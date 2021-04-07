44a11be2c692da55c642cc5542019c0cda569b9e
Ryan Lee <ryanlee059@gmail.com>
2021-04-06 23:41:17 -0700

    fixed some ID issues in HTML. Added function for save button and color-coding by hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+var timeText = JSON.parse(localStorage.getItem('timeText')) || [];
 
 
 
@@ -11,3 +11,43 @@ function currentDay () {
 
 currentDay();
 setInterval(currentDay, 1000);
+
+// Getting the current hour
+var currentTime = moment().format('H');
+
+// Checking current hour to hour of box
+$('*[id*="hour"]').each(function() {
+    var sectionTime = $(this).data('time');
+    if (sectionTime == currentTime) {
+        $(this).addClass('present');
+    } else if (sectionTime < currentTime) {
+        $(this).addClass('past');
+    } else {
+        $(this).addClass('future');
+    };
+});
+
+// Save button functionality
+$('*[id*="saveButton"]').each(function() {
+    $(this).on('click', function() {
+        var textInput = $(this).siblings('.description').val();
+        var blockHour = $(this).siblings('.description').data('time')
+        console.log(blockHour)
+
+        timeText.push({
+            hour: blockHour,
+            text: textInput
+        }); 
+
+        timeText.sort(function(a, b) {
+            return parseFloat(b.hour) - parseFloat(a.hour);
+        });
+
+        localStorage.setItem('timeText', JSON.stringify(timeText));
+    });
+});
+
+// Loading up saved information
+$('*[id*="hour"]').each(function() {
+    $(this).val(timeText.hour[sectionTime]);
+});
